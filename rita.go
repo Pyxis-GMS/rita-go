d0c15367dc25e663d54d166434096ce4a30186fc
Kriss Alner <cborgono@grupomilsabores.com>
2025-03-10 10:57:53 -0300

    nueva version

--- rita.go
+++ rita.go
@@ -301,7 +301,7 @@ func (c *RitaClient) SubEventSince(channel string, eventId string) (chan *RitaEv
 			return nil, UnknownError
 		}
 
-		go func() { //defer resp.Body.Close()
+		go func() {
 			for {
 				line, err := reader.ReadBytes('\n')
 				if err != nil {
